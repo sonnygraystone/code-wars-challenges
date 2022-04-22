3fff8048d82b627f9eff275d5f943345592d1c14
sonnygraystone <sonny.nguyen26@gmail.com>
2022-04-22 17:28:43 +1200

    decending kata
    Added additional way of completing kata as initial solution is rejected when submitting.

--- decending.js
+++ decending.js
@@ -1,9 +1,23 @@
+// function descendingOrder(n) {
+//   return Array.from(n.toString())
+//     .map(Number)
+//     .sort((a, b) => b - a)
+//     .join()
+//     .replaceAll(",", "");
+// }
+
 function descendingOrder(n) {
-  return Array.from(n.toString())
-    .map(Number)
+  var myArr = String(n)
+    .split("")
+    .map((num) => {
+      return Number(num);
+    });
+
+  var orderedArr = myArr
     .sort((a, b) => b - a)
     .join()
     .replaceAll(",", "");
+  return orderedArr;
 }
 
 console.log(descendingOrder(123456789));
