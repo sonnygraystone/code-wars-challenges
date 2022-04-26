dca091663afccb15c39222aac6a93b7c5b84bc8e
sonnygraystone <sonny.nguyen26@gmail.com>
2022-04-26 15:58:09 +1200

    function to validate emails using regex
    function to validate emails using regex

---
+++ helpfulcodeexamples/validateemail.js
@@ -0,0 +1,8 @@
+// the best way to validate an email address in JavaScript with a regular expression
+const validateEmail = (email) => {
+  return String(email)
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+};
